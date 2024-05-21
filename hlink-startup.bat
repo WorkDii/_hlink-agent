f451b67a4068f97bb270018c66b9586906514f93
Usman Kareemee <usman2535@gmail.com>
2024-05-21 22:27:22 +0700

    fix update bat

--- hlink-startup.bat
+++ hlink-startup.bat
@@ -1,3 +1,2 @@
-pm2 start dist/index.js --name=hlink-client --watch dist
-pm2 start dist/startUpdateAdapter.js --name=hlink-client-update --watch dist/startUpdateAdapter.js
-
+pm2 start dist/index.js --name=hlink-client --watch dist \
+pm2 start dist/updateAdapter.js --name=hlink-client-update --watch dist/updateAdapter.js
