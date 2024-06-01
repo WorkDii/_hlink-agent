e6f84990a03ce5e7dc8a8147a2b4d46e25b7e80c
usmancoe <usman2535@gmail.com>
2024-06-02 06:04:20 +0700

    build to exe

---
+++ build.js
@@ -0,0 +1,32 @@
+var rimraf = require("rimraf");
+
+function execShellCommand(cmd) {
+  const exec = require('child_process').exec;
+  return new Promise((resolve, reject) => {
+   exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+     console.warn(error);
+     }
+    resolve(stdout? stdout : stderr);
+   });
+  });
+ }
+async function main(params) {
+  rimraf.sync('dist');
+  console.log('remove dist');
+  rimraf.sync('out');
+  console.log('remove out');
+  await execShellCommand('tsc')
+  console.log('build tsc');
+  await execShellCommand('esbuild dist/index.js --bundle --platform=node --outfile=out/out.js --minify')
+  console.log('pack to one file');
+  await execShellCommand('node --experimental-sea-config sea-config.json')
+  console.log('pack to sea');
+  await execShellCommand(`node -e "require('fs').copyFileSync(process.execPath, 'out/hlink-client.exe')"`)
+  console.log('copy node to hlink-client.exe');
+  await execShellCommand(`npx postject out/hlink-client.exe NODE_SEA_BLOB out/sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2`)
+  console.log('inject sea-prep.blob to hlink-client.exe');
+}
+main().then(() => {
+  console.log('Build completed')
+})
